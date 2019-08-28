10236600b48beb6e40a182aaca7b5bcef6345b18
Thomas Cheronneau <thomas.cheronneau@mercedes-pay.com>
2019-08-28 11:34:32 +0200

    Refactor some code and fix some stuff

--- folder.go
+++ folder.go
@@ -2,8 +2,6 @@ package gapi
 
 import (
   "encoding/json"
-  "errors"
-  "io/ioutil"
   "fmt"
   "bytes"
 )
@@ -11,81 +9,26 @@ import (
 
 func (g *Grafana) GetFolders() ([]Folder, error) {
   folders := make([]Folder, 0)
-  req, err := g.newRequest("GET", "/api/folders", nil, nil)
-  if err != nil {
-    return folders, err
-  }
-  resp, err := g.Do(req)
-  if err != nil {
-    return folders, err
-  }
-  if resp.StatusCode != 200 {
-    return folders, errors.New(resp.Status)
-  }
-  data, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return folders, err
-  }
-  err = json.Unmarshal(data, &folders)
-  if err != nil {
-    return folders, err
-  }
-  return folders, err
+  return folders, g.getRequest("/api/folders",nil,nil,&folders)
 }
 
 
 func (g *Grafana) GetFolderByUid(uid string) (Folder, error) {
   folder := Folder{}
   url := fmt.Sprintf("/api/folders/%s", uid)
-  req, err := g.newRequest("GET",url,nil,nil)
-  if err != nil {
-    return folder, err
-  }
-  resp, err := g.Do(req)
-  if err != nil {
-    return folder, err
-  }
-  if resp.StatusCode != 200 {
-    return folder, errors.New(resp.Status)
-  }
-  data, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return folder, err
-  }
   tmp := Folder{}
-  err = json.Unmarshal(data, &tmp)
-  if err != nil {
-    return folder, err
-  }
+  err := g.getRequest(url,nil,nil,&tmp)
   folder = Folder(tmp)
-  return folder,err
+  return folder, err
 }
 
 func (g *Grafana) GetFolderById(id int) (Folder, error) {
   folder := Folder{}
   url := fmt.Sprintf("/api/folders/%d", id)
-  req, err := g.newRequest("GET",url,nil,nil)
-  if err != nil {
-    return folder, err
-  }
-  resp, err := g.Do(req)
-  if err != nil {
-    return folder, err
-  }
-  if resp.StatusCode != 200 {
-    return folder, errors.New(resp.Status)
-  }
-  data, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return folder, err
-  }
   tmp := Folder{}
-  err = json.Unmarshal(data, &tmp)
-  if err != nil {
-    return folder, err
-  }
+  err := g.getRequest(url,nil,nil,&tmp)
   folder = Folder(tmp)
-  return folder,err
+  return folder, err
 }
 
 func (g *Grafana) CreateFolder(title string) (error){
@@ -96,18 +39,7 @@ func (g *Grafana) CreateFolder(title string) (error){
   if err != nil {
     return err
   }
-  req, err := g.newRequest("POST", "/api/folders", nil, bytes.NewBuffer(data))
-  if err != nil {
-    return err
-  }
-  resp, err := g.Do(req)
-  if err != nil {
-    return err
-  }
-  if resp.StatusCode != 200 {
-    return errors.New(resp.Status)
-  }
-  return err
+  return g.postRequest("POST", "/api/folders", nil, bytes.NewBuffer(data))
 }
 
 func (g *Grafana) UpdateFolder(uid string, dataMap map[string]string) (error){
@@ -116,32 +48,10 @@ func (g *Grafana) UpdateFolder(uid string, dataMap map[string]string) (error){
     return err
   }
   url := fmt.Sprintf("/api/folders/%s",uid)
-  req, err := g.newRequest("PUT", url, nil, bytes.NewBuffer(data))
-  if err != nil {
-    return err
-  }
-  resp, err := g.Do(req)
-  if err != nil {
-    return err
-  }
-  if resp.StatusCode != 200 {
-    return errors.New(resp.Status)
-  }
-  return err
+  return g.postRequest("PUT", url, nil, bytes.NewBuffer(data))
 }
 
 func (g *Grafana) DeleteFolder(uid string) (error){
   url := fmt.Sprintf("/api/folders/%s",uid)
-  req, err := g.newRequest("DELETE", url, nil, nil)
-  if err != nil {
-    return err
-  }
-  resp, err := g.Do(req)
-  if err != nil {
-    return err
-  }
-  if resp.StatusCode != 200 {
-    return errors.New(resp.Status)
-  }
-  return err
+  return g.postRequest("DELETE", url, nil, nil)
 }
--- folderPermission.go
+++ folderPermission.go
@@ -2,8 +2,6 @@ package gapi
 
 import (
   "encoding/json"
-  "errors"
-  "io/ioutil"
   "fmt"
   "bytes"
 )
@@ -11,23 +9,7 @@ import (
 func (g *Grafana) GetFolderPermissions(uid string) ([]FolderPermission, error){
   perms := make([]FolderPermission,0)
   url := fmt.Sprintf("/api/folders/%s/permissions",uid)
-  req, err := g.newRequest("GET",url,nil,nil)
-  if err != nil {
-    return perms,err
-  }
-  resp, err := g.Do(req)
-  if err != nil {
-    return perms, err
-  }
-  if resp.StatusCode != 200 {
-    return perms, errors.New(resp.Status)
-  }
-  data, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return perms, err
-  }
-  err = json.Unmarshal(data, &perms)
-  return perms, err
+  return perms, g.getRequest(url,nil,nil,&perms)
 }
 
 func (g *Grafana) UpdateFolderPermissions(uid string, items []map[string]string) (error){
@@ -36,16 +18,5 @@ func (g *Grafana) UpdateFolderPermissions(uid string, items []map[string]string)
     return err
   }
   url := fmt.Sprintf("/api/folders/%s/permissions",uid)
-  req, err := g.newRequest("PUT", url, nil, bytes.NewBuffer(data))
-  if err != nil {
-    return err
-  }
-  resp, err := g.Do(req)
-  if err != nil {
-    return err
-  }
-  if resp.StatusCode != 200 {
-    return errors.New(resp.Status)
-  }
-  return err
+  return g.postRequest("PUT", url, nil, bytes.NewBuffer(data))
 }
